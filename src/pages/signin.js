67cfd5bf5576cdd4cdd0d4fa2a9fab2c4da78994
surajgupta3072 <surajgupta.3072@gmail.com>
2021-08-14 11:22:10 +0530

    deploy React App

---
+++ src/pages/signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '../components/Signin'
+
+const SignInPage = () => {
+    return (
+        <div>
+           <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage;
